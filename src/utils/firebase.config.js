d702006e1888785aba686fcfde2c74f5cc7fc8c7
rd39 <romain.doyen97@gmail.com>
2022-08-04 20:53:34 +0400

    make it better

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -3,11 +3,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_API_AUTH,
-  projectId: process.env.REACT_APP_API_ID,
-  storageBucket: process.env.REACT_APP_API_BUCKET,
-  messagingSenderId: process.env.REACT_APP_API_MESSAGING_ID,
-  appId: process.env.REACT_APP_API_APP_ID
+  authDomain: "designandcreation-react.firebaseapp.com",
+  projectId: "designandcreation-react",
+  storageBucket: "designandcreation-react.appspot.com",
+  messagingSenderId: "349146390731",
+  appId: "1:349146390731:web:2582afa414e1b623a43b4e"
 };
 
 const app = initializeApp(firebaseConfig);
