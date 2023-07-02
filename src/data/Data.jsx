0178ffad1ad9bd902504e543e4cd76846e8b672c
RomainDoyen <romain.doyen97@gmail.com>
2023-07-02 19:45:33 +0400

    Update link

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -4,6 +4,6 @@ export const Data = [
         title: "A propos de moi",
         date: "Romain Doyen, 25 septembre 2019",
         infos: "Salut, moi c'est Romain ! Passionné par le dessin depuis ma tendre enfance et je découvre plus tard l'informatique qui m'a donné l'idée de développer un site web pour y inclure mes créations. Bien sûr le site évoluera en fonction du temps.",
-        link: "Clique ici pour accéder à mon CV",
+        link: "Clique ici pour accéder à mon site perso !",
     },
 ];
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
                     <h2 className="article_view-title">{currentData[0].title}</h2>
                     <h3 className="article_view-subtitle">{currentData[0].date}</h3>
                     <p id="message" className="article_view-content">{currentData[0].infos}</p>
-                    <a id="link_cv" target="_blank" rel="noopener noreferrer" href="https://romaindoyen.netlify.app/"><p id="cv" className="link_cv">{currentData[0].link}</p></a>
+                    <a id="link_cv" target="_blank" rel="noopener noreferrer" href="https://romaindoyen.vercel.app/"><p id="cv" className="link_cv">{currentData[0].link}</p></a>
                 </div>
 		    </div>
             <Footer></Footer>
